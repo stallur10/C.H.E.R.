266b5f5b08a329612bd40c19c9e79051bd04d22d
Sahil K <skhunt125@gmail.com>
2024-04-06 19:24:17 -0500

    saved model

--- emotionclassification.py
+++ emotionclassification.py
@@ -14,102 +14,102 @@ Original file is located at
 # ENVIRONMENT SO THERE MAY BE MISSING LIBRARIES USED BY YOUR
 # NOTEBOOK.
 
-import os
-import sys
-from tempfile import NamedTemporaryFile
-from urllib.request import urlopen
-from urllib.parse import unquote, urlparse
-from urllib.error import HTTPError
-from zipfile import ZipFile
-import tarfile
-import shutil
+# import os
+# import sys
+# from tempfile import NamedTemporaryFile
+# from urllib.request import urlopen
+# from urllib.parse import unquote, urlparse
+# from urllib.error import HTTPError
+# from zipfile import ZipFile
+# import tarfile
+# import shutil
 
-CHUNK_SIZE = 40960
-DATA_SOURCE_MAPPING = 'facial-expression-dataset:https%3A%2F%2Fstorage.googleapis.com%2Fkaggle-data-sets%2F1060020%2F1782963%2Fbundle%2Farchive.zip%3FX-Goog-Algorithm%3DGOOG4-RSA-SHA256%26X-Goog-Credential%3Dgcp-kaggle-com%2540kaggle-161607.iam.gserviceaccount.com%252F20240405%252Fauto%252Fstorage%252Fgoog4_request%26X-Goog-Date%3D20240405T192318Z%26X-Goog-Expires%3D259200%26X-Goog-SignedHeaders%3Dhost%26X-Goog-Signature%3D89dccd16371c551592e0c74fd6267c634a9b59187de72c1b4a44e3512c1818440b644dc15c7bff4a7e516c22a8bfa5af9b8df185de6789d795512a9dd80752ce3a9dae97508154249bb9393104c87196ee939b00f990d0a084db54bddb0f65e584f11066d0f254d11eb8aa143a96d25c5ce8095eb445a29878f1484354edb562dc3558ec426adced114bc5fb6ecb304e24a254b3032da3315a8fbb32a04e389942cf9c2deadc79fd4dcd2f32fd7778c58fbed17efc2a189af42c92dd0d16c3306631afd5fffdf55869df0d8d7c3ee2af1623db6715080de3c359c60dac069f268be41582bf80b4360d7a0f5f26a8f90902041b684dfea319cf1e3ef94f3d836d'
+# CHUNK_SIZE = 40960
+# DATA_SOURCE_MAPPING = 'facial-expression-dataset:https%3A%2F%2Fstorage.googleapis.com%2Fkaggle-data-sets%2F1060020%2F1782963%2Fbundle%2Farchive.zip%3FX-Goog-Algorithm%3DGOOG4-RSA-SHA256%26X-Goog-Credential%3Dgcp-kaggle-com%2540kaggle-161607.iam.gserviceaccount.com%252F20240405%252Fauto%252Fstorage%252Fgoog4_request%26X-Goog-Date%3D20240405T192318Z%26X-Goog-Expires%3D259200%26X-Goog-SignedHeaders%3Dhost%26X-Goog-Signature%3D89dccd16371c551592e0c74fd6267c634a9b59187de72c1b4a44e3512c1818440b644dc15c7bff4a7e516c22a8bfa5af9b8df185de6789d795512a9dd80752ce3a9dae97508154249bb9393104c87196ee939b00f990d0a084db54bddb0f65e584f11066d0f254d11eb8aa143a96d25c5ce8095eb445a29878f1484354edb562dc3558ec426adced114bc5fb6ecb304e24a254b3032da3315a8fbb32a04e389942cf9c2deadc79fd4dcd2f32fd7778c58fbed17efc2a189af42c92dd0d16c3306631afd5fffdf55869df0d8d7c3ee2af1623db6715080de3c359c60dac069f268be41582bf80b4360d7a0f5f26a8f90902041b684dfea319cf1e3ef94f3d836d'
 
-KAGGLE_INPUT_PATH='/kaggle/input'
-KAGGLE_WORKING_PATH='/kaggle/working'
-KAGGLE_SYMLINK='kaggle'
+# KAGGLE_INPUT_PATH='/kaggle/input'
+# KAGGLE_WORKING_PATH='/kaggle/working'
+# KAGGLE_SYMLINK='kaggle'
 
-# !umount /kaggle/input/ 2> /dev/null
-shutil.rmtree('/kaggle/input', ignore_errors=True)
-os.makedirs(KAGGLE_INPUT_PATH, 0o777, exist_ok=True)
-os.makedirs(KAGGLE_WORKING_PATH, 0o777, exist_ok=True)
+# # !umount /kaggle/input/ 2> /dev/null
+# shutil.rmtree('/kaggle/input', ignore_errors=True)
+# os.makedirs(KAGGLE_INPUT_PATH, 0o777, exist_ok=True)
+# os.makedirs(KAGGLE_WORKING_PATH, 0o777, exist_ok=True)
 
-try:
-  os.symlink(KAGGLE_INPUT_PATH, os.path.join("..", 'input'), target_is_directory=True)
-except FileExistsError:
-  pass
-try:
-  os.symlink(KAGGLE_WORKING_PATH, os.path.join("..", 'working'), target_is_directory=True)
-except FileExistsError:
-  pass
+# try:
+#   os.symlink(KAGGLE_INPUT_PATH, os.path.join("..", 'input'), target_is_directory=True)
+# except FileExistsError:
+#   pass
+# try:
+#   os.symlink(KAGGLE_WORKING_PATH, os.path.join("..", 'working'), target_is_directory=True)
+# except FileExistsError:
+#   pass
 
-for data_source_mapping in DATA_SOURCE_MAPPING.split(','):
-    directory, download_url_encoded = data_source_mapping.split(':')
-    download_url = unquote(download_url_encoded)
-    filename = urlparse(download_url).path
-    destination_path = os.path.join(KAGGLE_INPUT_PATH, directory)
-    try:
-        with urlopen(download_url) as fileres, NamedTemporaryFile() as tfile:
-            total_length = fileres.headers['content-length']
-            print(f'Downloading {directory}, {total_length} bytes compressed')
-            dl = 0
-            data = fileres.read(CHUNK_SIZE)
-            while len(data) > 0:
-                dl += len(data)
-                tfile.write(data)
-                done = int(50 * dl / int(total_length))
-                sys.stdout.write(f"\r[{'=' * done}{' ' * (50-done)}] {dl} bytes downloaded")
-                sys.stdout.flush()
-                data = fileres.read(CHUNK_SIZE)
-            if filename.endswith('.zip'):
-              with ZipFile(tfile) as zfile:
-                zfile.extractall(destination_path)
-            else:
-              with tarfile.open(tfile.name) as tarfile:
-                tarfile.extractall(destination_path)
-            print(f'\nDownloaded and uncompressed: {directory}')
-    except HTTPError as e:
-        print(f'Failed to load (likely expired) {download_url} to path {destination_path}')
-        continue
-    except OSError as e:
-        print(f'Failed to load {download_url} to path {destination_path}')
-        continue
+# for data_source_mapping in DATA_SOURCE_MAPPING.split(','):
+#     directory, download_url_encoded = data_source_mapping.split(':')
+#     download_url = unquote(download_url_encoded)
+#     filename = urlparse(download_url).path
+#     destination_path = os.path.join(KAGGLE_INPUT_PATH, directory)
+#     try:
+#         with urlopen(download_url) as fileres, NamedTemporaryFile() as tfile:
+#             total_length = fileres.headers['content-length']
+#             print(f'Downloading {directory}, {total_length} bytes compressed')
+#             dl = 0
+#             data = fileres.read(CHUNK_SIZE)
+#             while len(data) > 0:
+#                 dl += len(data)
+#                 tfile.write(data)
+#                 done = int(50 * dl / int(total_length))
+#                 sys.stdout.write(f"\r[{'=' * done}{' ' * (50-done)}] {dl} bytes downloaded")
+#                 sys.stdout.flush()
+#                 data = fileres.read(CHUNK_SIZE)
+#             if filename.endswith('.zip'):
+#               with ZipFile(tfile) as zfile:
+#                 zfile.extractall(destination_path)
+#             else:
+#               with tarfile.open(tfile.name) as tarfile:
+#                 tarfile.extractall(destination_path)
+#             print(f'\nDownloaded and uncompressed: {directory}')
+#     except HTTPError as e:
+#         print(f'Failed to load (likely expired) {download_url} to path {destination_path}')
+#         continue
+#     except OSError as e:
+#         print(f'Failed to load {download_url} to path {destination_path}')
+#         continue
 
-print('Data source import complete.')
+# print('Data source import complete.')
 
-"""# **Hi Coders** Today We Are Going To Work Through A Problem Called **Face Emotion Recognition**.
-## This is an important topic in field of ML.We Used Our Model to detect human emotion based on their photos.
-It Mainly Consist of 7 emotions -->
+# """# **Hi Coders** Today We Are Going To Work Through A Problem Called **Face Emotion Recognition**.
+# ## This is an important topic in field of ML.We Used Our Model to detect human emotion based on their photos.
+# It Mainly Consist of 7 emotions -->
 
-`Happy` `Sad` `Anger` `Surprise` `Fear` `Neutral` `Disgust`.
+# `Happy` `Sad` `Anger` `Surprise` `Fear` `Neutral` `Disgust`.
 
-Algo Predict HumanEmotions.
+# Algo Predict HumanEmotions.
 
-I Have Divided This Project in two parts -
-1) Creating My Own Model.
+# I Have Divided This Project in two parts -
+# 1) Creating My Own Model.
 
-2) Using a pretrained Model.
+# 2) Using a pretrained Model.
 
-Obviously Second Work Going to work A Lot Better. I Have Not Even Created A Very Deep Learning Neural Network of my own.
-Like Follwing a Research paper but if you want to go I Will Suggest To Look at these  **Mollahosseini** and **Lopes**.Because My Semester Exams Are Coming 🥲.
-Maybe in Future I Will  Work On `Mollahosseini` CNN  Model .
+# Obviously Second Work Going to work A Lot Better. I Have Not Even Created A Very Deep Learning Neural Network of my own.
+# Like Follwing a Research paper but if you want to go I Will Suggest To Look at these  **Mollahosseini** and **Lopes**.Because My Semester Exams Are Coming 🥲.
+# Maybe in Future I Will  Work On `Mollahosseini` CNN  Model .
 
-On A Tough Intution He Did Follwoing Things-->
-## 1) Data Preprocessing:
-* Facial landmarks were extracted from the images.
-* The images were then resized to 48x48 pixels.
-* Augmentation techniques were applied to the data. Augmentation can involve operations like rotation, scaling, flipping, etc., to artificially increase the size of the training dataset and improve the model's generalization.
+# On A Tough Intution He Did Follwoing Things-->
+# ## 1) Data Preprocessing:
+# * Facial landmarks were extracted from the images.
+# * The images were then resized to 48x48 pixels.
+# * Augmentation techniques were applied to the data. Augmentation can involve operations like rotation, scaling, flipping, etc., to artificially increase the size of the training dataset and improve the model's generalization.
 
-## 2) Network Architecture:
-* In Conv Layers They Use Two Max Pooling Layers And And Two Conv2D Layer.
-* And After That They Use `Inception-Style Modules`With Filters With 1x1 and 3x3 and 5x5.
+# ## 2) Network Architecture:
+# * In Conv Layers They Use Two Max Pooling Layers And And Two Conv2D Layer.
+# * And After That They Use `Inception-Style Modules`With Filters With 1x1 and 3x3 and 5x5.
 
-### The Dataset we have Taken Is `Facial-Expression-dataset` on Kaggle
+# ### The Dataset we have Taken Is `Facial-Expression-dataset` on Kaggle
 
-### Importing The Neccessary Libraries
-"""
+# ### Importing The Neccessary Libraries
+# """
 
 import pandas as pd
 import numpy as np
@@ -131,8 +131,13 @@ print(tf.__version__)
 
 """### Load The Dataset `Train` And `Test`"""
 
-TRAIN_DIR="/kaggle/input/facial-expression-dataset/train"
-TEST_DIR="/kaggle/input/facial-expression-dataset/test"
+# DATASET LINK https://www.kaggle.com/datasets/aadityasinghal/facial-expression-dataset?select=test
+
+# TRAIN_DIR="/kaggle/input/facial-expression-dataset/train"
+# TEST_DIR="/kaggle/input/facial-expression-dataset/test"
+
+TRAIN_DIR="train"
+TEST_DIR="test"
 
 def load_datasets(directory):
 
@@ -297,7 +302,8 @@ early_stopping = EarlyStopping(monitor='val_loss',
                                patience=5,
                                restore_best_weights=True)
 
-history=model.fit(x=X_train,y=y_train,batch_size=32,epochs=100,validation_data=(X_test,y_test),callbacks=[early_stopping])
+# history=model.fit(x=X_train,y=y_train,batch_size=32,epochs=100,validation_data=(X_test,y_test),callbacks=[early_stopping])
+history=model.fit(x=X_train,y=y_train,batch_size=32,epochs=5,validation_data=(X_test,y_test),callbacks=[early_stopping])
 
 """### Plotting the accuracy and val_accuracy graph"""
 
@@ -364,7 +370,9 @@ prediction_label = le.inverse_transform([pred.argmax()])[0]
 print("Predicted Output:", prediction_label)
 plt.imshow(X_test[image_index].reshape(48, 48), cmap='gray');
 
-model.save('CNN_My_Model.h5')
+# model.save('savedModel')
+tf.saved_model.save(model, 'C:\\Users\\User\\OneDrive\\Desktop\\eve\\savedModel')
+
 
 """# Using Pretrained Network **Efficient Net**
 
@@ -414,7 +422,8 @@ X_train_rgb = np.squeeze(X_train_rgb, axis=-2)
 X_test_rgb = np.squeeze(X_test_rgb, axis=-2)
 
 batch_size = 32
-epochs = 40
+# epochs = 40
+epochs = 5
 
 train_generator = train_datagen.flow(X_train_rgb, y_train, batch_size=batch_size)
 test_generator = test_datagen.flow(X_test_rgb, y_test, batch_size=batch_size)
